6214621dd1d1b0589a100d32067441b3a34ff6e0
yuchi <chronictanvi@gmail.com>
2023-01-25 15:48:45 -0500

    Update CoordinateIndex.js
    look out for off by 1 error in array lengths

--- pages/CoordinateIndex.js
+++ pages/CoordinateIndex.js
@@ -2,7 +2,12 @@ import { useState } from "react";
 
 const postsArray = [
   ["this is a post", "this is another post", "this is the last post"],
-  ["this a down post", "this post is beneath that one", "this is rock bottom"],
+  ["this a down post", "this is the next down post", "this is last down post"],
+  [
+    "this is  post rock bottom post",
+    "this post is next rock bottom",
+    "this is final rock bottom",
+  ],
 ];
 
 const CoordinateIndex = () => {
@@ -12,26 +17,28 @@ const CoordinateIndex = () => {
   // as long as coordinate is longer than posts array.length we will keep incrementing
 
   const right = () => {
-    if (coordinates < postsArray.length - 1) {
-      setCoordinates([coordinates[0]][coordinates[1 + 1]]);
+    let currentRow = postsArray[coordinates[0]];
+    if (coordinates[1] < currentRow.length - 1) {
+      //creating a new array by taking 1st and second element of the corrdinates array. adding 1 to second elemnt of coordinates array.
+      setCoordinates([coordinates[0], coordinates[1] + 1]);
     }
   };
 
   const left = () => {
-    if (coordinates > 0) {
-      setCoordinates(coordinates - 1);
+    if (coordinates[1] > 0) {
+      setCoordinates([coordinates[0], coordinates[1] - 1]);
     }
   };
 
   const up = () => {
-    if (coordinates > 0) {
-      setCoordinates(coordinates - 1);
+    if (coordinates[0] > 0) {
+      setCoordinates([coordinates[0] - 1, coordinates[1]]);
     }
   };
 
   const down = () => {
-    if (coordinates > 0) {
-      setCoordinates(coordinates - 1);
+    if (coordinates[0] < postsArray.length - 1) {
+      setCoordinates([coordinates[0] + 1, coordinates[1]]);
     }
   };
 
@@ -41,7 +48,8 @@ const CoordinateIndex = () => {
         {" "}
         Current Coordinates {postsArray[coordinates[0]][coordinates[1]]}{" "}
       </h1>
-      <button onClick={left}>← </button> <button onClick={right}> →</button>
+      <button onClick={left}> ← </button>
+      <button onClick={right}> →</button>
       <button onClick={up}> ↑ </button>
       <button onClick={down}> ↓ </button>
     </div>
