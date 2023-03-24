81bc5dda756f2c77e6b49b5025d2be56c32e9e59
yuchi <chronictanvi@gmail.com>
2023-03-24 17:10:59 -0400

    Update Tutorial.tsx

--- src/Tutorial.tsx
+++ src/Tutorial.tsx
@@ -12,34 +12,37 @@ export default function Tutorial() {
           thoughts, and how they branch out.{" "}
         </p>
       </p>
+      <details>
+        <summary className=" text-xl pt-10 font-editorial text-left ">
+          Navigation Instructions →
+        </summary>
 
-      <h1 className=" text-xl pt-10 font-editorial text-left ">Navigation →</h1>
+        <div className="flex py-5">
+          <div className="flex-none">
+            <img className="w-28" src={imgUrl} alt="Keyboard Navigation"></img>
+          </div>
+          <div className="flex-1 text-left px-5 ">
+            <p className="font-editorial">
+              Use the keyboard keys to navigate to a square.{" "}
+            </p>
+            <p>Enter your text in the form.</p>
+            <p>Click anywhere on the webpage to submit.</p>
+          </div>
+        </div>
 
-      <div className="flex py-5">
-        <div className="flex-none">
-          <img className="w-28" src={imgUrl} alt="Keyboard Navigation"></img>
+        <div className="flex py-5">
+          <div className="flex-none px-10">
+            <img
+              className="object-center w-8 "
+              src={imgUrl1}
+              alt="Cursor Nav"
+            ></img>
+          </div>
+          <div className="flex-1 text-left px-5 ">
+            <p className="font-editorial">This is your current position </p>
+          </div>
         </div>
-        <div className="flex-1 text-left px-5 ">
-          <p className="font-editorial">
-            Use the keyboard keys to navigate to a square.{" "}
-          </p>
-          <p>Enter your text in the form.</p>
-          <p>Click anywhere on the webpage to submit.</p>
-        </div>
-      </div>
-
-      <div className="flex py-5">
-        <div className="flex-none px-10">
-          <img
-            className="object-center w-8 "
-            src={imgUrl1}
-            alt="Cursor Nav"
-          ></img>
-        </div>
-        <div className="flex-1 text-left px-5 ">
-          <p className="font-editorial">This is your current position </p>
-        </div>
-      </div>
+      </details>
     </div>
   );
 }
